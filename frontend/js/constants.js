31970e5fe550b63992d7fcf1cff77ce9991506e3
MooseKnucklesNFT <gajulabsbusiness@gmail.com>
2023-01-31 19:58:03 -0500

    website 3

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,10 +1,10 @@
 const contractAddress = "0xF17d5968f225c8adEAc5A51661CEc81fA4e40456"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Moose Knuckles NFT Project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = 'Moose Knuckles aims to garner the best community in Web3!';
+const h1_presale_coming_soon = "NFT Drop Coming Soon!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
